940dbc525f165ff04ef4c9fedada8cebd57526cc
siikabb <loukkola@outlook.com>
2022-12-14 13:20:31 +0200

    bugfixejä

--- Ville/game/Flask.py
+++ Ville/game/Flask.py
@@ -10,62 +10,6 @@ app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-# ilkan esimerkit!
-#@app.route('/continents')
-#def continents():
-#    sql = f'''SELECT DISTINCT continent
-#              FROM country'''
-#    cursor = db.get_conn().cursor(dictionary=True)
-#    cursor.execute(sql)
-#    result = cursor.fetchall()
-#    return json.dumps(result)
-
-
-#@app.route('/countries/<continent>')
-#def countries_by_continent(continent):
-#    sql = f'''SELECT iso_country, name
-#              FROM country
-#              WHERE continent = %s'''
-#    cursor = db.get_conn().cursor(dictionary=True)
-#    cursor.execute(sql, (continent,))
-#    result = cursor.fetchall()
-#    return json.dumps(result)
-
-
-#@app.route('/airports/<country>')
-#def airports_by_country(country):
-#    sql = f'''SELECT ident, name, latitude_deg, longitude_deg
-#              FROM airport
-#              WHERE iso_country = %s'''
-#    cursor = db.get_conn().cursor(dictionary=True)
-#    cursor.execute(sql, (country,))
-#    result = cursor.fetchall()
-#    return json.dumps(result)
-
-
-#@app.route('/airport/<icao>')
-#def airport(icao):
-#    sql = f'''SELECT name, latitude_deg, longitude_deg
-#              FROM airport
-#              WHERE ident=%s'''
-#    cursor = db.get_conn().cursor(dictionary=True)
-#    cursor.execute(sql, (icao,))
-#    result = cursor.fetchone()
-#    return json.dumps(result)
-#------------------------------------------------------------------------
-
-
-#Oma koodi
-
-#@app.route('/username/<name>')
-#def username(name):
-#    sql = f'''INSERT INTO game(screen_name)
-#     VALUES("{name}") '''
-#    kursori = db.get_conn().cursor()
-#    kursori.execute(sql)
-#    return 'user has been added'
-
-#cheking if name is already in use, continuing with it
 userName = 'empty'
 
 @app.route('/username/<name>')
